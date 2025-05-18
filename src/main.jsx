27e2a5addd28e9adc92a985401152a8346397a67
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2025-05-17 23:46:37 -0600

    project setup + folder structure setup, creates placeholder store and wires up main.jsx so it holds provider that provides redux store globally and wrap app with browserRouter

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { App } from "./App";
+
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
